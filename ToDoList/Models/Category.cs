c15acbbc6433e68eeb68bc983d3a62223f329b7f
Julien L <julienlenaz96@gmail.com>
2023-09-19 21:50:36 -0400

    Added new category model and tests

---
+++ ToDoList/Models/Category.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+
+namespace ToDoList.Models
+{
+  public class Category
+  {
+    private static List<Category> _instances = new List<Category> {};
+    public string Name { get; set; }
+    public int Id { get; }
+    public List<Item> Items { get; set; }
+  }
+}
