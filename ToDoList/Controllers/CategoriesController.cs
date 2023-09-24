0e77324940278033d3662297f59af5527014abde
Julien L <julienlenaz96@gmail.com>
2023-09-23 22:07:05 -0400

    Added new Show view, created dictionary  method and route

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -29,5 +29,17 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
+      [HttpGet("/categories/{id}")]
+  public ActionResult Show(int id)
+  {
+    Dictionary<string, object> model = new Dictionary<string, object>();
+    Category selectedCategory = Category.Find(id);
+    List<Item> categoryItems = selectedCategory.Items;
+    model.Add("category", selectedCategory);
+    model.Add("items", categoryItems);
+    return View(model);
+  }
+
+
   }
 }
