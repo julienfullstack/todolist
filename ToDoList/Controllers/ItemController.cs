7c01593d04226f1300081898774c3db87b49ce74
Julien L <julienlenaz96@gmail.com>
2023-09-23 22:19:48 -0400

    Updated controllers

--- ToDoList/Controllers/ItemController.cs
+++ ToDoList/Controllers/ItemController.cs
@@ -6,43 +6,23 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+
+    [HttpGet("/categories/{categoryId}/items/new")]
+    public ActionResult New(int categoryId)
     {
-      return View();
+       Category category = Category.Find(categoryId);
+       return View(category);
     }
 
-    [HttpPost("/items")]
-    public ActionResult Create(string description)
+    [HttpGet("/categories/{categoryId}/items/{itemId}")]
+    public ActionResult Show(int categoryId, int itemId)
     {
-      Item myItem = new Item(description);
-      return RedirectToAction("Index");
+      Item item = Item.Find(itemId);
+      Category category = Category.Find(categoryId);
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      model.Add("item", item);
+      model.Add("category", category);
+      return View(model);
     }
-
-    [HttpGet("/items/{id}")]
-    public ActionResult Show(int id)
-    {
-      Item foundItem = Item.Find(id);
-      return View(foundItem);
-    }
-
-  [HttpGet("/categories/{categoryId}/items/{itemId}")]
-  public ActionResult Show(int categoryId, int itemId)
-  {
-    Item item = Item.Find(itemId);
-    Category category = Category.Find(categoryId);
-    Dictionary<string, object> model = new Dictionary<string, object>();
-    model.Add("item", item);
-    model.Add("category", category);
-    return View(model);
-  }
-
-  [HttpGet("/categories/{categoryId}/items/new")]
-  public ActionResult New(int categoryId)
-  {
-     Category category = Category.Find(categoryId);
-     return View(category);
-  }
-
   }
 }
