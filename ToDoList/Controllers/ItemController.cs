00d3ffe4d33aeeaa29530eec5f2e9364e3e17770
Julien L <julienlenaz96@gmail.com>
2023-09-23 22:12:44 -0400

    Updated Item controller, view

--- ToDoList/Controllers/ItemController.cs
+++ ToDoList/Controllers/ItemController.cs
@@ -19,19 +19,23 @@ namespace ToDoList.Controllers
       return RedirectToAction("Index");
     }
 
-    [HttpPost("/items/delete")]
-    public ActionResult DeleteAll()
-    {
-      Item.ClearAll();
-      return View();
-    }
-
-        [HttpGet("/items/{id}")]
+    [HttpGet("/items/{id}")]
     public ActionResult Show(int id)
     {
       Item foundItem = Item.Find(id);
       return View(foundItem);
     }
+
+  [HttpGet("/categories/{categoryId}/items/{itemId}")]
+  public ActionResult Show(int categoryId, int itemId)
+  {
+    Item item = Item.Find(itemId);
+    Category category = Category.Find(categoryId);
+    Dictionary<string, object> model = new Dictionary<string, object>();
+    model.Add("item", item);
+    model.Add("category", category);
+    return View(model);
+  }
 
   }
 }
