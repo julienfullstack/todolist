05f587548db85d3844b0501b30040625e6f0e333
Julien L <julienlenaz96@gmail.com>
2023-09-23 22:10:22 -0400

    Updated Item views and controller

--- ToDoList/Controllers/ItemController.cs
+++ ToDoList/Controllers/ItemController.cs
@@ -6,13 +6,6 @@ namespace ToDoList.Controllers
 {
   public class ItemsController : Controller
   {
-    [HttpGet("/items")]
-    public ActionResult Index()
-    {
-      List<Item> allItems = Item.GetAll();
-      return View(allItems);
-    }
-
     [HttpGet("/items/new")]
     public ActionResult CreateForm()
     {
