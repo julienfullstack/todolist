9d64a63211add60c83e01b0df054d6bf9b08e428
Julien L <julienlenaz96@gmail.com>
2023-09-25 20:54:22 -0400

    Added database access

--- ToDoList/Program.cs
+++ ToDoList/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using ToDoList.Models;
 
 namespace ToDoList
 {
@@ -10,6 +11,8 @@ namespace ToDoList
       WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
       builder.Services.AddControllersWithViews();
+
+      DBConfiguration.ConnectionString = builder.Configuration["ConnectionStrings:DefaultConnection"];
 
       WebApplication app = builder.Build();
 
